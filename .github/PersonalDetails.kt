7cee6d7ca7a1bec63d9248f307ec372e03e51048
hanumankindkumar-gtm <hanumankindkumar@gmail.com>
2026-06-01 21:58:13 +0530

    Delete .github directory

--- .github/PersonalDetails.kt
+++
@@ -1,8 +0,0 @@
-package com.akprojects.copyclipper.data
-
-data class PersonalDetails(
-    val fullName: String = "",
-    val idNumber: String = "",
-    val dob: String = "",
-    val gender: String = ""
-)
